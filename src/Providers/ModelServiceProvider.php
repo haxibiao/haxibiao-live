830e12d5c66a7c8ee506c96feb0ccb5b374352be
曾达威 <zengdawei@haxibiao.com>
2020-05-25 14:46:44 +0800

    暂时把liveRoom直接放到User Model中, 绕过 Macroable 问题

--- src/Providers/ModelServiceProvider.php
+++ src/Providers/ModelServiceProvider.php
@@ -2,21 +2,13 @@
 
 namespace Haxibiao\Live\Providers;
 
-use App\User;
-use Haxibiao\Live\Models\LiveRoom;
 use Illuminate\Support\ServiceProvider;
 
 class ModelServiceProvider extends ServiceProvider
 {
     public function register()
     {
-        $this->registerUserMacros();
+
     }
 
-    protected function registerUserMacros()
-    {
-         User::macro('liveRoom', function () {
-             return $this->hasOne(LiveRoom::class, 'anchor_id');
-         });
-    }
 }
