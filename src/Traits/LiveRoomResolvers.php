0da268bf0c4adf370bf674ed8cb952af12c3f6f7
ivan <ivan@haxibiao.com>
2020-07-03 16:10:48 +0800

    重命名namspace兼容composer autoload

--- src/Traits/LiveRoomResolvers.php
+++ src/Traits/LiveRoomResolvers.php
@@ -8,7 +8,7 @@ use Haxibiao\Live\Events\NewLiveRoomMessage;
 use Haxibiao\Live\Events\NewUserComeIn;
 use Haxibiao\Live\Events\UserGoOut;
 use Haxibiao\Live\LiveRoom;
-use haxibiao\user\HXBUser;
+use Haxibiao\User\HXBUser;
 use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Collection;
 use Illuminate\Database\Eloquent\Model;
