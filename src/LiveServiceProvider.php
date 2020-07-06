42b660e8009a371fe126d27c005f11b75fd8db59
蔡志刚 <caizhigang@haxibiao.com>
2020-07-06 17:53:03 +0800

    不存在的Command不注册

--- src/LiveServiceProvider.php
+++ src/LiveServiceProvider.php
@@ -147,7 +147,6 @@ class LiveServiceProvider extends ServiceProvider
         $this->commands([
             InstallCommand::class,
             PublishCommand::class,
-            CleanUpLiveRoom::class,
             UninstallCommand::class,
         ]);
     }
