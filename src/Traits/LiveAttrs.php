3888e64a7b74e60c8a489377143d0eaf3e2b6b17
曾达威 <zengdawei@haxibiao.com>
2020-07-28 15:53:03 +0800

    迁移 room attr 到 live

--- src/Traits/LiveAttrs.php
+++ src/Traits/LiveAttrs.php
@@ -2,7 +2,10 @@
 
 namespace Haxibiao\Live\Traits;
 
+use Haxibiao\Live\Live;
+use Haxibiao\Live\LiveRoom;
 use Illuminate\Support\Facades\Redis;
+use Illuminate\Support\Facades\Storage;
 
 /**
  * 直播秀的属性
@@ -31,5 +34,48 @@ trait LiveAttrs
     {
         $count = json_decode(Redis::get($this->redis_key), true);
         return $count ? count($count) : 0;
+    }
+
+    /**
+     * 直播间默认的直播秀
+     */
+    public function getLiveAttribute()
+    {
+        //FIXME: 断流3分钟内别关闭live status 可以连回来
+        $live = $this->lives()->where('status', '>=', 0)->latest('id')->first();
+        //没有，开一个，关了，再开一个
+        if (is_null($live)) {
+            $live = Live::create([
+                'user_id'      => $this->user_id,
+                'live_room_id' => $this->id,
+            ]);
+            return $live;
+        }
+        return $live;
+    }
+
+    /**
+     * 直播间标题
+     */
+    public function getTitleAttribute()
+    {
+        return $this->live->title;
+    }
+
+    public function getPushUrlAttribute(): string
+    {
+        return LiveRoom::prefix . $this->push_stream_url . "/" . $this->push_stream_key;
+    }
+
+    public function getPullUrlAttribute(): string
+    {
+        $live = $this->live;
+        return LiveRoom::prefix . config('live.live_pull_domain') . "/" . config('live.app_name') . "/"
+        . $live->stream_name;
+    }
+
+    public function getCoverUrlAttribute(): string
+    {
+        return Storage::cloud()->url($this->cover) ?? 'https://dtzq-1251052432.cos.ap-shanghai.myqcloud.com/2020-03-25/u%3A980235-screenshot-15-20-45-1192x746.jpg';
     }
 }
